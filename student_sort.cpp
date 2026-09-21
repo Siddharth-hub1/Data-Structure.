8712f8da8631918a5fcae356a94e397e0f74ca76
Siddharth Yadav <sidharthyadav455@gmail.com>
2026-09-21 13:23:46 +0530

    Clean up comments in student_sort.cpp
    Removed comments for input, sorting, and display sections.

--- student_sort.cpp
+++ student_sort.cpp
@@ -4,14 +4,12 @@ using namespace std;
 int main() {
     int marks[5];
 
-    // Input marks
     cout << "Enter marks of 5 students:\n";
     for (int i = 0; i < 5; i++) {
         cout << "Student " << i + 1 << ": ";
         cin >> marks[i];
     }
 
-    // Sort in descending order
     for (int i = 0; i < 5; i++) {
         for (int j = i + 1; j < 5; j++) {
             if (marks[i] < marks[j]) {
@@ -22,7 +20,6 @@ int main() {
         }
     }
 
-    // Display marks
     cout << "\nMarks from highest to lowest:\n";
     for (int i = 0; i < 5; i++) {
         cout << marks[i] << endl;
